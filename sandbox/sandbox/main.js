d864ad7e1d4aa435103fe37cf5170d636665b8f4
akirameikyou <shin.gi.tai.meikyoushisui@gmail.com>
2026-05-20 21:45:43 +0900

    Update main.js

--- sandbox/sandbox/main.js
+++ sandbox/sandbox/main.js
@@ -1,1 +1,33 @@
+import {
+createGlobe,
+renderScene
+}
+from "./renderGlobe.js";
 
+const globe =
+createGlobe(
+document.getElementById(
+"globeViz"
+)
+);
+
+const events = [
+
+{
+title:"東京",
+lat:35.68,
+lng:139.69
+},
+
+{
+title:"ハワイ",
+lat:21.30,
+lng:-157.85
+}
+
+];
+
+renderScene(
+globe,
+events
+);
