4f6ba58631aff77928bc3a6ca96677dc10243bfb
akirameikyou <shin.gi.tai.meikyoushisui@gmail.com>
2026-05-25 21:17:09 +0900

    Update geoLayers.js

--- data/geoLayers.js
+++ data/geoLayers.js
@@ -1,24 +1,25 @@
 export const geoLayers = {
 
-usa_civil_war:{
-type:'Feature',
-properties:{
-color:'rgba(255,94,94,0.22)',
-stroke:'rgba(255,94,94,0.75)'
-},
-geometry:{
-type:'Polygon',
-coordinates:[[
-[-98,37],
-[-92,37],
-[-84,36],
-[-76,34],
-[-78,29],
-[-86,27],
-[-95,29],
-[-102,32],
-[-98,37]
-]]
-}
-},
+  usa_civil_war: {
+    type: 'Feature',
+    properties: {
+      color: 'rgba(255,94,94,0.22)',
+      stroke: 'rgba(255,94,94,0.75)'
+    },
+    geometry: {
+      type: 'Polygon',
+      coordinates: [[
+        [-98,37],
+        [-92,37],
+        [-84,36],
+        [-76,34],
+        [-78,29],
+        [-86,27],
+        [-95,29],
+        [-102,32],
+        [-98,37]
+      ]]
+    }
+  }
 
+};
